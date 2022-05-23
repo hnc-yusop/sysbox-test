e21ef6c7097103aac50587f7cee5ec726787f2a1
hnc-yusop <83211009+hnc-yusop@users.noreply.github.com>
2022-05-23 23:21:09 +0900

    Update k8s-pull.sh

--- k8s-pull.sh
+++ k8s-pull.sh
@@ -1,10 +1,22 @@
-#!/usr/bin/shk8s_version=$1
-flannel_version=$2# Start containerd
+#!/usr/bin/sh
+
+k8s_version=$1
+flannel_version=$2
+
+# Start containerd
 containerd &
 containerd_pid=$!
-sleep 2# Pull k8s images
-kubeadm config images list --kubernetes-version=$k8s_version | xargs -n 1 ctr images pull# Pull Flannel CNI images
+sleep 2
+
+# Pull k8s images
+kubeadm config images list --kubernetes-version=$k8s_version | xargs -n 1 ctr images pull
+
+# Pull Flannel CNI images
 ctr images pull docker.io/rancher/mirrored-flannelcni-flannel-cni-plugin:v1.0.0
-ctr images pull quay.io/coreos/flannel:$flannel_version# List pulled images
-ctr images list# Stop containerd
+ctr images pull quay.io/coreos/flannel:$flannel_version
+
+# List pulled images
+ctr images list
+
+# Stop containerd
 kill $containerd_pid
